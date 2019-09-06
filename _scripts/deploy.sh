df53e75fe119cb03e8e0d9af15e519fb9e502981
allen-woods <allen.woods@flatironschool.com>
2019-09-06 05:14:45 -0700

    test: Travis CI deploy, try 2

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -x
 echo "the deploy step has started..."
-if [ $TRAVIS_BRANCH == 'master' ] ; then
 
-  # Initialize a new git repo in _site, and push it to our server.
-  cd _site
-  git init
+# Initialize a new git repo in _site, and push it to our server.
+cd _site
+git init
 
-  git remote add deploy "deploy@$VPS_IP:/var/www/thesupertask.com"
-  git config user.name "Travis CI"
-  git config user.email "allen@thesupertask.com"
+git remote add deploy "deploy@thesupertask.com:/var/www/thesupertask.com"
+git config user.name "Travis CI"
+git config user.email "allen@thesupertask.com"
 
-  git add .
-  git commit -m "Deploy"
-  git push --force deploy master
-else
-  echo "Not Deployed: branch must be master"
-fi
+git add .
+git commit -m "Deploy"
+git push --force deploy master
+
 echo "the deploy step has ended"
