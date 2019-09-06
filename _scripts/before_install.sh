c3ceb0acc4430c7e863fed6e358a4420b066e72f
allen-woods <allen.woods@flatironschool.com>
2019-09-06 05:51:22 -0700

    test: Travis CI deploy key

--- _scripts/before_install.sh
+++ _scripts/before_install.sh
@@ -9,7 +9,7 @@ openssl aes-256-cbc -K $encrypted_a9f5e5e6d65e_key -iv $encrypted_a9f5e5e6d65e_i
 # Delete encrypted SSH key
 rm travis_ci_deploy_thesupertask_key.enc
 eval "$(ssh-agent -s)" # Use SSH commands
-chmod 600 ./deploy_key # Access restrictions
+chmod 600 ./deploy_key # Remove access restrictions
 # Allow user "deploy" to login without prompt
 echo -e "Host $VPS_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 # Use deploy_key as preferred SSH identity
