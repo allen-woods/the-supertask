91f0336fe93135d69d34a0a50b2bc9db2ce75a15
allen-woods <allen.woods@flatironschool.com>
2019-09-06 16:51:01 -0700

    test: Reconfigure after_success bash script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,17 +2,26 @@
 set -x
 echo "the deploy step has started..."
 
-# Initialize a new git repo in _site, and push it to our server.
-# mkdir _site && cd _site
+eval "$(ssh-agent -s)" # Use SSH commands
+chmod 600 ./deploy_key # Remove access restrictions
+# Allow user "deploy" to login without prompt
+echo -e "Host $VPS_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+# Use deploy_key as preferred SSH identity
+ssh-add ./deploy_key
 
+# Initilize the repo
 git init
+
+# Whitelist only the items we want to push
 echo "!index.html" >> .gitignore
 echo "!/src/**" >> .gitignore
 
-git remote add deploy "ssh://deploy@$VPS_IP:/var/www/thesupertask.com.git"
+# Add the deploy and configure it
+git remote add deploy "deploy@$VPS_IP:/var/www/thesupertask.com.git"
 git config user.name "Travis CI"
 git config user.email "allen@thesupertask.com"
 
+# Stage, commit, and push
 git add .
 git commit -m "Deploy"
 git push --set-upstream deploy master
