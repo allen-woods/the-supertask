3747cfc5caa63284ea3c349411ec876a5f248349
allen-woods <allen.woods@flatironschool.com>
2019-09-06 07:04:17 -0700

    test: Travis CI over SSH, try 2

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,14 +5,16 @@ echo "the deploy step has started..."
 # Initialize a new git repo in _site, and push it to our server.
 # mkdir _site && cd _site
 
-# git init
+git init
+echo "!index.html" >> .gitignore
+echo "!/src/**" >> .gitignore
 
 git remote add deploy "ssh://deploy@$VPS_IP:/var/www/thesupertask.com.git"
 # git config user.name "Travis CI"
 # git config user.email "allen@thesupertask.com"
 
-# git add .
-# git commit -m "Deploy"
+git add .
+git commit -m "Deploy"
 git push --set-upstream deploy master
 
 echo "the deploy step has ended"
