c52113ca660f9c51e4ad2aec5efa2062cd0d8de0
allen-woods <allen.woods@flatironschool.com>
2019-09-06 17:04:14 -0700

    test: Verifying before_install

--- _scripts/before_install.sh
+++ _scripts/before_install.sh
@@ -8,9 +8,3 @@ set -x # show output of commands
 openssl aes-256-cbc -K $encrypted_a9f5e5e6d65e_key -iv $encrypted_a9f5e5e6d65e_iv -in travis_ci_deploy_thesupertask_key.enc -out ./deploy_key -d
 # Delete encrypted SSH key
 rm travis_ci_deploy_thesupertask_key.enc
-eval "$(ssh-agent -s)" # Use SSH commands
-chmod 600 ./deploy_key # Remove access restrictions
-# Allow user "deploy" to login without prompt
-echo -e "Host $VPS_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-# Use deploy_key as preferred SSH identity
-ssh-add ./deploy_key
