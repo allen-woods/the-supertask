cabbb94c4694a38a4e2791328c738210c967d964
allen-woods <allen.woods@flatironschool.com>
2019-09-30 15:38:45 -0700

    refactored proxy

--- mongo-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "MongoDB: creating $DB_USERNAME user on $MONGO_INITDB_DATABASE database"
-
-mongo ${MONGO_INITDB_DATABASE} \
-    -u ${MONGO_INITDB_ROOT_USERNAME} \
-    -p ${MONGO_INITDB_ROOT_PASSWORD} \
-    --authenticationDatabase admin \
-    --eval "db.createUser({user: '$DB_USERNAME', pwd: '$DB_PASSWORD', roles:['readWrite']});"
