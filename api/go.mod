9bd6884cd48e9388ff7099f9fb86a1b5de93f884
allen-woods <allen.woods@flatironschool.com>
2020-09-18 07:12:10 -0700

    Prepared new folder grpc for microservice definitions.

--- api/go.mod
+++ api/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/99designs/gqlgen v0.12.2
 	github.com/vektah/gqlparser/v2 v2.0.1
 	go.mongodb.org/mongo-driver v1.4.1
+	google.golang.org/grpc v1.32.0 // indirect
 )
--- api/graph/schema.resolvers.go
+++ api/graph/schema.resolvers.go
@@ -6,17 +6,39 @@ package graph
 import (
 	"context"
 	"fmt"
+	"log"
 
-	generated1 "github.com/allen-woods/the-supertask/api/graph/generated"
-	model1 "github.com/allen-woods/the-supertask/api/graph/model"
+	graph_gen "github.com/allen-woods/the-supertask/api/graph/generated"
+	graph_model "github.com/allen-woods/the-supertask/api/graph/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc"
 )
 
-func (r *mutationResolver) SignUpUser(ctx context.Context, input *model1.NewUser) (*model1.User, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) SignUpUser(ctx context.Context, input *graph_model.NewUser) (*graph_model.User, error) {
+	// Define any user data to be sent over gRPC.
+
+	// Dial the gRPC server for the User model.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatal("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// c := pb.NewSignUpClient
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	// defer cancel()
+
+	// Send the data
+	// r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	// if err != nil {
+	// 	log.Fatalf("could not sign up: %v", err)
+	// }
+
+	// Process any results returned by the gRPC server.
+	// log.Printf("Signed up: %s", r.GetMessage())
 }
 
-func (r *mutationResolver) LogInUser(ctx context.Context, email string, password string) (*model1.User, error) {
+func (r *mutationResolver) LogInUser(ctx context.Context, email string, password string) (*graph_model.User, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
@@ -28,19 +50,19 @@ func (r *mutationResolver) DeleteUser(ctx context.Context, id primitive.ObjectID
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Me(ctx context.Context) (*model1.User, error) {
+func (r *queryResolver) Me(ctx context.Context) (*graph_model.User, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Users(ctx context.Context) ([]*model1.User, error) {
+func (r *queryResolver) Users(ctx context.Context) ([]*graph_model.User, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-// Mutation returns generated1.MutationResolver implementation.
-func (r *Resolver) Mutation() generated1.MutationResolver { return &mutationResolver{r} }
+// Mutation returns graph_gen.MutationResolver implementation.
+func (r *Resolver) Mutation() graph_gen.MutationResolver { return &mutationResolver{r} }
 
-// Query returns generated1.QueryResolver implementation.
-func (r *Resolver) Query() generated1.QueryResolver { return &queryResolver{r} }
+// Query returns graph_gen.QueryResolver implementation.
+func (r *Resolver) Query() graph_gen.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
