23f76ee1ba49217c5d2277b7e5870046270254be
allen-woods <allen.woods@flatironschool.com>
2020-10-06 22:28:58 -0700

    Updating absolute paths in init scripts.

--- mongo/init/init.sh
+++ mongo/init/init.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Bring in our environment variables.
-source /usr/local/etc/custom-mongo-init/mongo-init-env-vars.sh
+source /usr/local/etc/custom-mongo-init/includes/mongo-init-env-vars.sh
 
 # echo $(echo -n "${MDB_JS//[[:space:]]/}")
 
 # Create a mongod instance for us to run our initialization against.
-mongod --port $MONGO_PORT \
+mongod --bind_ip_all \
+--port $MONGO_PORT \
 --fork \
 --logpath=/var/log/mongodb/mongod \
+--directoryperdb \
+--eval $MDB_JS
 
 # Create a mongo client that injects MDB_JS and initializes our superuser.
 mongo $MONGO_INITDB_DATABASE \
--- redis/init/init.sh
+++ redis/init/init.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+source /usr/local/etc/custom-redis-init/includes/redis-init-env-vars.sh
+
+# TBD down here
