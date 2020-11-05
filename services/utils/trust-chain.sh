3c5d2dcdd81175f6f2108603cfdbac567566fb59
allen-woods <allen.woods@flatironschool.com>
2020-11-04 21:19:50 -0800

    Working on  utility.

--- services/utils/trust-chain.sh
+++ services/utils/trust-chain.sh
@@ -2,6 +2,14 @@
 
 # Work in progress!
 
+# Currently following:
+# https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
+
+argon_pass = "$(gen random string here)"
+argon_salt = "$(gen random string here)"
+argon_hash = "$(echo -n $argon_pass | argon2 $argon_salt -id -t 12 -m 16 -p 1 -l 32 -r -v 13)"
+argon_encd = "$(echo -n $argon_pass | argon2 $argon_salt -id -t 12 -m 16 -p 1 -l 32 -e -v 13)"
+
 # Generate a Base64 string from the plain text argument $1.
 # Store the result in a file named `cert-phrase` in the current directory.
 echo $(echo -n "${$1}" | \
