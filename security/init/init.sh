2a80786e4fa89ff2c3a293cbcd2c9b1f61ba2109
allen-woods <allen.woods@flatironschool.com>
2020-11-06 07:40:50 -0800

    Work in progress on `trust` function, previously gen_trust_chain.

--- security/init/init.sh
+++ security/init/init.sh
@@ -3,3 +3,48 @@
 #!/bin/sh
 
 # Work in progress!
+
+# HashiCorp recommends maintaining root certificate outside of Vault and
+# providing Vault a signed intermediate CA.
+#
+# Following tutorial, we:
+function trust {
+  # Generate a randomized 32 character salt using [:graph:]
+  # character set.
+  #
+  # NOTE: This becomes base64 encoded with no padding.
+  #
+  local saltStr="$( \
+  tr -cd '[:alnum:][:punct:]' < /dev/urandom | \
+  fold -w32 | \
+  head -n1)"
+
+  # Generate a random password using [:graph:] character set
+  # and length ranging from 128 to 8192 characters.
+  #
+  # NOTE: This becomes base64 encoded with no padding.
+  #
+  local passStr="$( \
+  tr -cd '[:alnum:][:punct:]' < /dev/urandom | \
+  fold -w$(jot -w %i -r 1 128 8192) | \
+  head -n1)"
+
+  # Generate the encoded hash string from Argon2.
+  local encdStr=$(echo -n $passStr | argon2 $saltStr -id -t 5 -m 16 -p 4 -l 32 -e)
+
+  # Generate directory structure for our secure files.
+  #
+  # NOTE: add strict chmod and chown settings
+  #
+  mkdir -p $HOME/tls/certs
+  mkdir -p $HOME/tls/private
+
+  # Move current directory to $HOME.
+  cd $HOME
+
+  # Store password in `phrase.raw` file inside root directory.
+  echo $passStr > phrase.raw
+
+  # Store encoded hash from Argon2 inside `phrase.enc` file.
+  echo $encdStr > ./tls/certs/phrase.enc
+}
