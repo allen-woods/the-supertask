a02a46c35904decea124de4c152465e3d51ba029
allen-woods <allen.woods@flatironschool.com>
2020-11-20 18:40:34 -0800

    Transcribed implementation by Alex Edwards.

--- security/init/config/trust/main.go
+++ security/init/config/trust/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
+)
+
+var (
+	ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
 )
 
 type params struct {
@@ -17,7 +25,7 @@ type params struct {
 	keyLength   uint32
 }
 
-// This is a work in progress.
+// This is a work in progress. The transcribed source seen here will be refactored
 // For more information about this implementation of Argon2id in Go, please read the following article:
 // https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
 func main() {
@@ -29,12 +37,17 @@ func main() {
 		keyLength:   32,
 	}
 
-	hash, err := generateFromPassword("passwordString", p)
+	encodedHash, err := generateFromPassword("passwordString", p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(hash)
+	match, err := comparePasswordAndHash("passwordString", encodedHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Match: %v\n", match)
 }
 
 func generateFromPassword(password string, p *params) (encodedHash string, err error) {
@@ -61,4 +74,53 @@ func generateRandomBytes(n uint32) ([]byte, error) {
 	}
 
 	return b, nil
+}
+
+func comparePasswordAndHash(password, encodedHash string) (match bool, err error) {
+	p, salt, hash, err := decodeHash(encodedHash)
+	if err != nil {
+		return false, err
+	}
+
+	otherHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func decodeHash(encodedHash string) (p *params, salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, ErrInvalidHash
+	}
+
+	var version int
+	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if version != argon2.Version {
+		return nil, nil, nil, ErrIncompatibleVersion
+	}
+
+	p = &params{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	salt, err = base64.RawStdEncoding.DecodeString(vals[4])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.saltLength = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.keyLength = uint32(len(hash))
+
+	return p, salt, hash, nil
 }
