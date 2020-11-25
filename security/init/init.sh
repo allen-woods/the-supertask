b45e345f981a45ed803bc6e47f2a6c28ae92f205
allen-woods <allen.woods@flatironschool.com>
2020-11-25 03:47:34 -0800

    Working on init of Vault.

--- security/init/init.sh
+++ security/init/init.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
 
 function init_vault {
-  local res=$(curl \
-  --request PUT \
-  --data @$(pwd)/json/sys_init.json \
-  http://127.0.0.1:8200/v1/sys/init)
+  # Extended params:
+  #   "pgp_keys": [
+  #     "file1.asc",
+  #     "file2.asc",
+  #     "file3.asc"
+  #   ],
+  #   "root_token_pgp_key": "fileX.asc",
+  local api_v1="http://127.0.0.1:8200/v1"
+  local sys_init=/usr/local/init/sys_init.json
+  local sys_init_addr=$api_v1/sys/init
 
-  echo "${res}"
+  echo "** Begin Initialization **"
+
+  curl \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -X PUT \
+  -d @$sys_init \
+  $sys_init_addr | jq
+
+  local res=false
+
+  echo "Verifying initialization..."
+  while [ "$(echo -n $res | grep -w false)" = "false" ]; do
+    res=$(curl \
+    -s
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -X GET \
+    $sys_init_addr)
+  done
+
+  echo "** Init OK! **"
+  echo "$res" | jq
 }
