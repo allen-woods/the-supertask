eb06bf5a1bb28d955fb47db07f26feadcacc3bfd
allen-woods <allen.woods@flatironschool.com>
2020-11-27 23:14:36 -0800

    Working on parsing engine of PGP automation.

--- security/init/scripts/init.sh
+++ security/init/scripts/init.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# 1. Generate Passphrases.
-# 2. Generate shell script containing Passphrases.
-# 3. Generate Keys.
-# 4. Copy keys into Vault
-# 5. Pass exported keys into Vault init.
-# Utility function for generating random strings.
-function random_string {
+function rand_fmt {
   if [ ! -z $2] && [ ! -z $3 ]
   then
     if [ $2 -eq $3 ]
@@ -25,31 +19,11 @@ function random_string {
   )
 }
 
-# Utility function for parsing end exporting generated keys.
-function export_keys {
-  local n=1
-  cd ${2:-${HOME}/.gnupg}
-
-  for file in ${1:-/root/.gnupg/openpgp-revocs.d/*}
-  do
-    echo "Exporting: key $n"
-    
-    gpg2 \
-    --export \
-    "$(basename "$file" | cut -f 1 -d '.')" | \
-    base64 > "key$n.asc"
-
-    $n=$(($n + 1))
-  done
-}
-
 function init_vault {
-  cd ${HOME}/.gnupg
+  export GNUPGHOME="$(mktemp -d)"
+  cd $GNUPGHOME
 
-  # local min=[ $3 -lt $2 ] && $3 || $2
-  # local max=[ $2 -gt $3 ] && $2 || $3
-
-  
+  set -A PHRASE
   local BATCH_FILENAME='.'"$(rand_fmt a-f0-9 16 16)"''
   local n=0
 
@@ -66,34 +40,21 @@ function init_vault {
       '"Name-Real: "'
       '"Name-Email: "'
       '"Name-Comment: "'
-      '"Expire-Date: 0"'
-      '"%commit"'' >> ${BATCH_FILENAME}
+      '"Expire-Date: 0"'' >> ${BATCH_FILENAME}
 
     $n=$(($n + 1))
   done
 
-  echo "%echo Done." >> ${BATCH_FILENAME}
+  echo ''"%commit"'
+  '"%echo Done."'' >> ${BATCH_FILENAME}
   
   gpg2 \
   --verbose \
   --batch \
   --gen-key ${BATCH_FILENAME}
 
-  # This is how to get the hex of each key:
-  # for file in /root/.gnupg/openpgp-revocs.d/*; do echo "$(basename "$file" | cut -f 1 -d '.')"; done
-  
-  # This is how to append to a string:
-  # str="$str $new_word"
-
-  # This is how to parse each word value of hex within appended string:
-  # my_blank_str="these are some words to echo out in my loop"
-  # for w in $my_blank_str; do echo "$w"; done
-
   # echo -e "5\ny\n" | gpg2 --command-fd 0 --expert --edit-key somename@site.com trust;
-  
-  # We capture the output so we can capture the hex strings for export command.
   local gpg_out=$(gpg2 --list-keys)
-  
   # gpg2 -e -a -r somename@site.com ${BATCH_FILENAME}
 
   # First, we must parse HEXCODE from `gpg_out`.
@@ -108,14 +69,11 @@ function init_vault {
   '"These passphrases will not be persisted."'
   '"Please keep secure copies for later use."'
   '"Provide them to GPG2's prompt manually."'
+
+  '"1: $(rand_fmt [:alnum:][:punct:])"'
+  '"2: $(rand_fmt [:alnum:][:punct:])"'
+  '"3: $(rand_fmt [:alnum:][:punct:])"'
   '
-
-  $n=0
-
-  while [ $n -lt ${1:-4} ]
-  do
-    echo "$n: $PHRASE[$n]"
-  done
 
   # Use multi-line string literal (POSIX doesn't support heredoc).
   echo '
