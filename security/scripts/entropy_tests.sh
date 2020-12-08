0539cb8b7a43be1f4f9ce3bbe3291dba07404e54
allen-woods <allen.woods@flatironschool.com>
2020-12-08 06:02:30 -0800

    DieHarder 3.31.1 successfully patched in entropy_tests script.

--- security/scripts/entropy_tests.sh
+++ security/scripts/entropy_tests.sh
@@ -56,75 +56,62 @@ function entropy_tests {
   cd /dieharder_src/*
   ./autogen.sh
   
-  # rpm -i --nodeps dieharder-3.31.1-1.src.rpm
+  # * * * * * Perform Surgery on Broken Install * * * * *
 
+  # ---------------- DIEHARDER.SPEC[.IN]
+  
   # Patch line 16 to fix "Failed build dependencies" error message.
   sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec
   sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec.in
-
   # Patch line 129 to fix "Macro expanded in comment" warning message.
   sed -i '129s/.*/# /' ./dieharder.spec
   sed -i '129s/.*/# /' ./dieharder.spec.in
 
+  # ---------------- BITS.C
+
   # Patch line 414 to fix "bufbits set but unused" error message.
   sed -i '414s/.*/ bdelta = sizeof(unsigned int)\*CHAR_BIT - bufbits;/' ./libdieharder/bits.c
-
   # Patch line 421 to remove `rmax_bits` where `bufbits` should be.
   sed -i '421s/.*/   printf(\"bufbits = %d  bdelta = %d\\n\",bufbits,bdelta);/' ./libdieharder/bits.c
-  
-  # # Insert new line 1213 to create pointer variable.
-  # sed -i '1213i unsigned int \*ptr;' ./libdieharder/bits.c
-  
-  # # Insert new line 1216 to assign address of `tmp` to `ptr`
-  # sed -i '1216i \ ptr = \&tmp;' ./libdieharder/bits.c
 
+  # ---------------- DAB_DCT.C
 
-  # # Patch line 1217 to fix "while referencing tmp" error message.
-  # sed -i '1217s/.*/ btmp = (char \*)(ptr + 2);/' ./libdieharder/bits.c
+  # Insert new line 39 to define missing `M_PI` constant.
+  sed -i '39i #define M_PI     3.14159265358979323846' ./libdieharder/dab_dct.c
+  # Patch line 290 to remove unused `pos` variable.
+  sed -i '290s/.*/\ /' ./libdieharder/dab_dct.c
+  # Patch line 302 to remove unused `pos` variable.
+  sed -i '302s/.*/\ /' ./libdieharder/dab_dct.c
 
-  # Errors that break build
-  # File: bits.c
-  # Line: 407 - "bufbits set but unused"
-  #    Fix?: Changed rmax_bits to bufbits.
-  # Line: 1212 - "while referencing tmp"
-  #    Fix?: Added *ptr and changed &tmp to ptr.
-  #    Note: This fix should resolve two remaining issues below.
-  # Line: 1247 - "array subscript 8 is out of bounds (btmp[0])"
-  # Line: 1265 - "array subscript 9 is out of bounds (btmp[1])"
-  #
-  # File: dab_dct.c
-  # Line: 251 - "M_PI undeclared"
-  # Line: 274 - "M_PI undeclared"
-  # Line: 289 - "pos set but unused"
-  #
-  # File: Makefile
-  # Line: 412
-  # Line: 594
+  # ---------------- LIBDIEHARDER.H
+
+  # Insert new line 261 to create `uint` typedef.
+  sed -i '261i typedef unsigned int uint;' ./include/dieharder/libdieharder.h
+  # Insert new line 262 to clean up formatting.
+  sed -i '262i \ ' ./include/dieharder/libdieharder.h
 
   make install
 
-  # rpmbuild --rebuild dieharder-3.31.1-1.src.rpm
+  echo "$sec_header"
+  echo "Running Cryptographic Entropy Test: DieHarder"
+  echo "  * Entropy Avail: $(cat /proc/sys/kernel/random/entropy_avail)"
+  echo "  * Entropy Total: $(cat /proc/sys/kernel/random/poolsize)"
+  echo "$sec_header"
 
-  # echo "$sec_header"
-  # echo "Running Cryptographic Entropy Test: DieHarder"
-  # echo "  * Entropy Avail: $(cat /proc/sys/kernel/random/entropy_avail)"
-  # echo "  * Entropy Total: $(cat /proc/sys/kernel/random/poolsize)"
-  # echo "$sec_header"
+  dieharder -a
 
-  # dieharder -a
-
-  # echo "$sec_header"
-  # echo "  Test Complete: DieHarder"
-  # echo "$sec_header"
+  echo "$sec_header"
+  echo "  Test Complete: DieHarder"
+  echo "$sec_header"
   
-  # # Remove pakcages needed to build ENT and DieHarder.
-  # apk.static del \
-  # chrpath \
-  # gsl \
-  # gsl-dev \
-  # gsl-doc \
-  # libtool \
-  # make \
-  # rpm-dev \
-  # build-base
+  # Remove pakcages needed to build ENT and DieHarder.
+  apk.static del \
+  chrpath \
+  gsl \
+  gsl-dev \
+  gsl-doc \
+  libtool \
+  make \
+  rpm-dev \
+  build-base
 }
