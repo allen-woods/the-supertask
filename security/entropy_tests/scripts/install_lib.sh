02646e62e730459602500caf73c1cd511a8e395a
allen-woods <allen.woods@flatironschool.com>
2021-01-27 02:06:39 -0800

    Converted cert authority code to install_lib format.

--- security/entropy_tests/scripts/install_lib.sh
+++ security/entropy_tests/scripts/install_lib.sh
@@ -179,131 +179,131 @@ apk_static_add_build_base() {
   echo -e "\033[7;33mAdded Build Base\033[0m" 1>&5
 }
 apk_static_add_chrpath() {
-  apk.static add chrpath
-  echo -e "\033[7;33mAdded Chrpath\033[0m"
+  apk.static add chrpath 1>&4
+  echo -e "\033[7;33mAdded Chrpath\033[0m" 1>&5
 }
 apk_static_add_gsl() {
-  apk.static add gsl
-  echo -e "\033[7;33mAdded Gsl\033[0m"
+  apk.static add gsl 1>&4
+  echo -e "\033[7;33mAdded Gsl\033[0m" 1>&5
 }
 apk_static_add_gsl_dev() {
-  apk.static add gsl-dev
-  echo -e "\033[7;33mAdded Gsl Dev\033[0m"
+  apk.static add gsl-dev 1>&4
+  echo -e "\033[7;33mAdded Gsl Dev\033[0m" 1>&5
 }
 apk_static_add_haveged() {
-  apk.static add haveged
-  echo -e "\033[7;33mAdded Haveged\033[0m"
+  apk.static add haveged 1>&4
+  echo -e "\033[7;33mAdded Haveged\033[0m" 1>&5
 }
 apk_static_add_libtool() {
-  apk.static add libtool
-  echo -e "\033[7;33mAdded Libtool\033[0m"
+  apk.static add libtool 1>&4
+  echo -e "\033[7;33mAdded Libtool\033[0m" 1>&5
 }
 apk_static_add_openrc() {
-  apk.static add openrc
-  echo -e "\033[7;33mAdded OpenRC\033[0m"
+  apk.static add openrc 1>&4
+  echo -e "\033[7;33mAdded OpenRC\033[0m" 1>&5
 }
 apk_static_add_rng_tools() {
-  apk.static add rng-tools
-  echo -e "\033[7;33mAdded RNG Tools\033[0m"
+  apk.static add rng-tools 1>&4
+  echo -e "\033[7;33mAdded RNG Tools\033[0m" 1>&5
 }
 apk_static_add_rpm_dev() {
-  apk.static add rpm-dev
-  echo -e "\033[7;33mAdded RPM Dev\033[0m"
+  apk.static add rpm-dev 1>&4
+  echo -e "\033[7;33mAdded RPM Dev\033[0m" 1>&5
 }
 rc_update_add_haveged() {
-  rc-update add haveged
-  echo -e "\033[7;33mScheduled Haveged Using rc-update\033[0m"
+  rc-update add haveged 1>&4
+  echo -e "\033[7;33mScheduled Haveged Using rc-update\033[0m" 1>&5
 }
 create_tmp_test_make_dir() {
-  mkdir -pm 0700 /tmp/test/make
-  echo -e "\033[7;33mCreated /tmp/test/make Directory\033[0m"
+  mkdir -pm 0700 /tmp/test/make 1>&4
+  echo -e "\033[7;33mCreated /tmp/test/make Directory\033[0m" 1>&5
 }
 change_dir_to_tmp_test_make() {
-  cd /tmp/test/make
-  echo -e "\033[7;33mChanged Current Directory to /tmp/test/make\033[0m"
+  cd /tmp/test/make 1>&4
+  echo -e "\033[7;33mChanged Current Directory to /tmp/test/make\033[0m" 1>&5
 }
 download_ent_from_fourmilab() {
-  wget http://www.fourmilab.ch/random/random.zip
-  echo -e "\033[7;33mDownloaded ENT from FourmiLab\033[0m"
+  wget http://www.fourmilab.ch/random/random.zip 1>&4
+  echo -e "\033[7;33mDownloaded ENT from FourmiLab\033[0m" 1>&5
 }
 extract_ent_zip_file() {
-  unzip random.zip
-  echo -e "\033[7;33mExtracted ENT Zip Archive\033[0m"
+  unzip random.zip 1>&4
+  echo -e "\033[7;33mExtracted ENT Zip Archive\033[0m" 1>&5
 }
 build_ent_using_make() {
-  make
-  echo -e "\033[7;33mRan Make to Build ENT\033[0m"
+  make 1>&4
+  echo -e "\033[7;33mRan Make to Build ENT\033[0m" 1>&5
 }
 move_ent_files() {
-  mv ./ent /tmp/test/
-  echo -e "\033[7;33mMoved ENT Files from $(pwd)/ent to /tmp/test\033[0m"
+  mv ./ent /tmp/test/ 1>&4
+  echo -e "\033[7;33mMoved ENT Files from $(pwd)/ent to /tmp/test\033[0m" 1>&5
 }
 delete_ent_zip_file() {
-  rm -f random.zip
-  echo -e "\033[7;33mDeleted ENT Zip Archive\033[0m"
+  rm -f random.zip 1>&4
+  echo -e "\033[7;33mDeleted ENT Zip Archive\033[0m" 1>&5
 }
 create_home_rpmbuild_dir() {
-  mkdir -pm 0700 $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-  echo -e "\033[7;33mCreated ${HOME}/rpmbuild Directory\033[0m"
+  mkdir -pm 0700 $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} 1>&4
+  echo -e "\033[7;33mCreated ${HOME}/rpmbuild Directory\033[0m" 1>&5
 }
 chown_root_home_rpmbuild_dir() {
-  chown root:root $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-  echo -e "\033[7;33mChanged Ownership of ${HOME}/rpmbuild to Root User\033[0m"
+  chown root:root $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} 1>&4
+  echo -e "\033[7;33mChanged Ownership of ${HOME}/rpmbuild to Root User\033[0m" 1>&5
 }
 generate_home_rpmmacros_file() {
-  echo '%_topdir %(echo $HOME)/rpmbuild' >> $HOME/.rpmmacros
-  echo -e "\033[7;33mGenerated ${HOME}/.rpmmacros File\033[0m"
+  echo '%_topdir %(echo $HOME)/rpmbuild' >> $HOME/.rpmmacros 1>&4
+  echo -e "\033[7;33mGenerated ${HOME}/.rpmmacros File\033[0m" 1>&5
 }
 create_dieharder_src_dir() {
-  mkdir -pm 0700 /dieharder_src
-  echo -e "\033[7;33mCreated /dieharder_src Directory\033[0m"
+  mkdir -pm 0700 /dieharder_src 1>&4
+  echo -e "\033[7;33mCreated /dieharder_src Directory\033[0m" 1>&5
 }
 chown_root_dieharder_src_dir() {
-  chown root:root /dieharder_src
-  echo -e "\033[7;33mChanged Ownership of /dieharder_src to Root User\033[0m"
+  chown root:root /dieharder_src 1>&4
+  echo -e "\033[7;33mChanged Ownership of /dieharder_src to Root User\033[0m" 1>&5
 }
 download_dieharder_latest_release() {
-  wget -c --quiet http://webhome.phy.duke.edu/~rgb/General/dieharder/dieharder.tgz -O - | tar -xz -C /dieharder_src/
-  echo -e "\033[7;33mDownloaded Latest Release of DieHarder to /dieharder_src\033[0m"
+  wget -c http://webhome.phy.duke.edu/~rgb/General/dieharder/dieharder.tgz -O - | tar -xz -C /dieharder_src/ 1>&4
+  echo -e "\033[7;33mDownloaded Latest Release of DieHarder to /dieharder_src\033[0m" 1>&5
 }
 change_dir_to_dieharder_src() {
-  cd /dieharder_src/*
-  echo -e "\033[7;33mChanged Current Directory to $(pwd)\033[0m"
+  cd /dieharder_src/* 1>&4
+  echo -e "\033[7;33mChanged Current Directory to $(pwd)\033[0m" 1>&5
 }
 run_autogen_script() {
-  ./autogen.sh
-  echo -e "\033[7;33mRan Autogen Script\033[0m"
+  ./autogen.sh 1>&4
+  echo -e "\033[7;33mRan Autogen Script\033[0m" 1>&5
 }
 patch_line_16_dieharder_spec() {
-  sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec
-  echo -e "\033[7;33mPatched Line 16 of $(pwd)/dieharder.spec\033[0m"
+  sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec 1>&4
+  echo -e "\033[7;33mPatched Line 16 of $(pwd)/dieharder.spec\033[0m" 1>&5
 }
 patch_line_129_dieharder_spec() {
-  sed -i '129s/.*/# /' ./dieharder.spec
-  echo -e "\033[7;33mPathed Line 129 of $(pwd)/dieharder.spec\033[0m"
+  sed -i '129s/.*/# /' ./dieharder.spec 1>&4
+  echo -e "\033[7;33mPathed Line 129 of $(pwd)/dieharder.spec\033[0m" 1>&5
 }
 patch_line_16_dieharder_spec_in() {
-  sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec.in
-  echo -e "\033[7;33mPatched Line 16 of $(pwd)/dieharder.spec.in\033[0m"
+  sed -i '16s/.*/chrpath gsl-dev/' ./dieharder.spec.in 1>&4
+  echo -e "\033[7;33mPatched Line 16 of $(pwd)/dieharder.spec.in\033[0m" 1>&5
 }
 patch_line_129_dieharder_spec_in() {
-  sed -i '129s/.*/# /' ./dieharder.spec.in
-  echo -e "\033[7;33mPathed Line 129 of $(pwd)/dieharder.spec.in\033[0m"
+  sed -i '129s/.*/# /' ./dieharder.spec.in 1>&4
+  echo -e "\033[7;33mPathed Line 129 of $(pwd)/dieharder.spec.in\033[0m" 1>&5
 }
 insert_m_pi_constant_libdieharder_h() {
-  sed -i '66i #define M_PI    3.14159265358979323846' ./include/dieharder/libdieharder.h
-  echo -e "\033[7;33mInserted Missing Constant M_PI into $(pwd)/include/dieharder/libdieharder.h\033[0m"
+  sed -i '66i #define M_PI    3.14159265358979323846' ./include/dieharder/libdieharder.h 1>&4
+  echo -e "\033[7;33mInserted Missing Constant M_PI into $(pwd)/include/dieharder/libdieharder.h\033[0m" 1>&5
 }
 insert_uint_type_def_libdieharder_h() {
-  sed -i '262i typedef unsigned int uint;' ./include/dieharder/libdieharder.h
-  echo -e "\033[7;33mInserted Missing uint Type Definition into $(pwd)/include/dieharder/libdieharder.h\033[0m"
+  sed -i '262i typedef unsigned int uint;' ./include/dieharder/libdieharder.h 1>&4
+  echo -e "\033[7;33mInserted Missing uint Type Definition into $(pwd)/include/dieharder/libdieharder.h\033[0m" 1>&5
 }
 insert_new_line_262_libdieharder_h() {
-  sed -i '263i \ ' ./include/dieharder/libdieharder.h
-  echo -e "\033[7;33mInserted New Line 262 into $(pwd)/include/dieharder/libdieharder.h\033[0m"
+  sed -i '263i \ ' ./include/dieharder/libdieharder.h 1>&4
+  echo -e "\033[7;33mInserted New Line 262 into $(pwd)/include/dieharder/libdieharder.h\033[0m" 1>&5
 }
 compile_dieharder_using_make_install() {
-  echo -e "\033[7;33mCompiling DieHarder Test Suite. Please Wait..."
-  make install
-  echo -e "\033[7;33mRan Make to Build Installation of DieHarder Test Suite\033[0m"
+  echo -e "\033[7;33mCompiling DieHarder Test Suite. Please Wait..." 1>&5
+  make install 1>&4
+  echo -e "\033[7;33mRan Make to Build Installation of DieHarder Test Suite\033[0m" 1>&5
 }
