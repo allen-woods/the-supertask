ee8920f1a03b80709c18ec7c4183933bb33981bf
allen-woods <allen.woods@flatironschool.com>
2021-03-22 01:52:26 -0700

    Removed unnecessary files and directories.

--- security/shared/scripts/escape_string.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Utility function for escaping special characters in a string.
-# Usage:
-# escape_string <string_to_escape>
-escape_string() {
-  [[ ! -z $1 ]] && echo -n $(echo $1 | sed -e 's/[[:punct:]]/\\&/g')
-}
